b96c6c4bafaeb7757c2e1054c687d32c2b82066b
Steopa2001 <moraru495@gmail.com>
2025-07-18 18:06:57 +0200

    feat: run for loop to generate emails

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,19 @@
-axios.get(`https://flynn.boolean.careers/exercises/api/random/mail`).then((resp) => {
-    console.log(resp.data.response);
-});
+//recupero elementi dal DOM
+const emailList = document.getElementById('email-list');
+const generaBtn = document.getElementById('genera-btn');
+
+//creo una funzione dove vado a generare 10 email 
+function generaEmail() {
+
+//pulisco lista
+  emailList.innerHTML = ''; 
+
+//eseguo ciclo for per generare 10 email
+for (i = 0; i < 10; i++) {
+        axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((resp) => {
+            const email = resp.data.response;
+            emailList.innerHTML += `<li>${email}</li>`;
+        })
+    }
+}
+
